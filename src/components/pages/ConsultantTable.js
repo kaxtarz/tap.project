9e570d55352c692768531b65c5c961107eb4b5c1
LMeloTapQA <lmelo@tapqa.com>
2020-06-16 11:31:15 -0500

    addclienttable

---
+++ src/components/pages/ConsultantTable.js
@@ -0,0 +1,73 @@
+
+import axios from 'axios';
+import React, { Component } from 'react';
+
+class ConsultantTable extends React.Component {
+constructor(props) {
+      super(props)
+
+        this.state = {
+          consultants: []
+        }
+
+
+}
+
+  componentDidMount() {
+    axios.get(`http://localhost:8080/consultantarray`)
+      .then(res => {
+        const consultants = res.data;
+        this.setState({ consultants });
+      })
+  }
+
+  renderTableHeader() {
+   return (
+<>
+   <th>ID</th>
+   <th>Consultant</th>
+   <th>Role</th>
+   <th>Status</th>
+</>
+   );
+
+
+
+
+
+
+  }
+
+    renderTableData() {
+      return this.state.consultants.map((consultant, index) => {
+        const { consultantId, consultantName, consultantRole, consultantStatus } = consultant; //destructuring
+        return (
+          <tr key={consultantId}>
+            <td>{consultantId}</td>
+            <td>{consultantName}</td>
+            <td>{consultantRole}</td>
+            <td>{consultantStatus}</td>
+
+          </tr>
+        )
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <h1 id='title'>tap|QA</h1>
+          <h3 id='title'>Consultant Roster</h3>
+          <table id='consultants'>
+            <tbody>
+              <tr>{this.renderTableHeader()}</tr>
+              {this.renderTableData()}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+
+
+}
+export default  ConsultantTable;
